7ccf50f2ca7e5c94d13f4e06c230b6892809c5d1
izikandrw <isaac.stennett@gmail.com>
2017-06-01 23:22:01 -1000

    initial commit

---
+++ _chrome.py
@@ -0,0 +1,37 @@
+from dragonfly import (Grammar, AppContext, MappingRule, Dictation, Key, Text, Integer, Mimic)
+
+context = AppContext(executable="chrome")
+grammar = Grammar("chrome", context=context)
+noSpaceNoCaps = Mimic("\\no-caps-on") + Mimic("\\no-space-on")
+
+rules = MappingRule(
+    name = "chrome",
+    mapping = {
+      "edit": Key("w-a"),
+      "reload" : Key("f5"),
+      "open": Key("escape, o"),
+      "new tab": Key("t"),
+      "search tabs": Key("T"),
+      "find": Key("c-f"),
+      "console": Key("cs-j"),
+      "developer tools": Key("cs-i"),
+      "close tab": Key("c-w"),
+      "escape": Key('escape'),
+      },
+    extras = [
+        Dictation("text"),
+        Integer("n", 0, 20000),
+      ],
+    defaults = {
+      "n" : 1
+      }
+    )
+
+grammar.add_rule(rules)
+
+grammar.load()
+
+def unload():
+  global grammar
+  if grammar: grammar.unload()
+  grammar = None
---
+++ _git.py
@@ -0,0 +1,31 @@
+from dragonfly import (Grammar, AppContext, MappingRule, Dictation, Key, Text, Integer, Mimic)
+
+#context = AppContext(executable="chrome")
+#grammar = Grammar("chrome", context=context)
+grammar = Grammar("git")
+
+rules = MappingRule(
+    name = "git",
+    mapping = {
+      "get status": Text("git status") + Key("enter"),
+      "get add all": Text("git add .") + Key("enter"),
+      "get commit <text>": Text("git commit -m \"%(text)s\""),
+      "get push": Text("git push") + Key("enter")
+    },
+    extras = [
+        Dictation("text"),
+        Integer("n", 0, 20000),
+      ],
+    defaults = {
+      "n" : 1
+      }
+    )
+
+grammar.add_rule(rules)
+
+grammar.load()
+
+def unload():
+  global grammar
+  if grammar: grammar.unload()
+  grammar = None
