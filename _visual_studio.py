bc1862dc7175b34c0000893639fb0759255843c5
izikandrw <isaac.stennett@gmail.com>
2017-07-28 10:56:33 -1000

    Visual Studio solution Explorer command

--- _visual_studio.py
+++ _visual_studio.py
@@ -2,14 +2,17 @@ from dragonfly import (Grammar, AppContext, MappingRule, Dictation, Key, Text, I
 
 context = AppContext(executable="devenv")
 grammar = Grammar("visualstudio", context=context)
-#grammar = Grammar("visualstudio")
+
 noSpaceNoCaps = Mimic("\\no-caps-on") + Mimic("\\no-space-on")
 
 rules = MappingRule(
     name = "visualStudio",
     mapping = {
       "debug" : Key("f5"),
-      "stop debug": Key("s-f5")
+      "stop debug": Key("s-f5"),
+      "open file": Key("c-comma"),
+      "search": Key("c-f"),
+      "solution explorer": Key("ca-l")
       },
     extras = [
         Dictation("text"),
