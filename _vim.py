8dbfd1cc6185d89de58b204f977eb8f03b1d18c0
Isaac Stennett <izikandrw@gmail.com>
2017-08-04 23:39:26 -1000

    changing some vim commands

--- _vim.py
+++ _vim.py
@@ -17,6 +17,7 @@ grammarInsert = Grammar("Vim insert grammar", context=context)
 
 def enable_insert_mode(char):
     global grammarCommand
+    print("in enable_insert_mode")
     global grammarInsert
     grammarCommand.disable()
     grammarInsert.enable()
@@ -32,7 +33,6 @@ def enable_command_mode():
 commandMode = MappingRule(
     mapping={
         "tim": Text("vim") + Key("enter"),
-        # Commands and keywords:
         "append [text]": Function(enable_insert_mode, char="a"),
         "append [text] (to|at) end [of line]": Function(enable_insert_mode, char="A"),  # @IgnorePep8
         "copy [(line|lines)]": Key("y, y"),
@@ -40,23 +40,36 @@ commandMode = MappingRule(
         "insert [text] at beginning [of line]": Function(enable_insert_mode, char="I"),  # @IgnorePep8
         "insert line before": Function(enable_insert_mode, char="O"),
         "insert line after": Function(enable_insert_mode, char="o"),
+        "page up": Key("c-b, c-b"),
+        "page down": Key("c-f"),
         "paste [(line|lines)]": Key("p"),
         "save": Key("colon, w, enter"),
         "save and exit": Key("colon, x, space"),
         "save as": Key("colon, w, space"),
         "undo": Function(enable_command_mode) + Key("u"),
-        "yank [(line|lines)]": Key("d, d"),
+        "cut [(line|lines)]": Key("d, d"),
         "quit": Function(enable_command_mode) + Text(":q") + Key("enter"),
         "force quit": Function(enable_command_mode) + Text(":q!") + Key("enter"),
+        #Navigation
+        "top": Function(enable_command_mode) + Key("H"),
+        "mid": Function(enable_command_mode) + Key("M"),
+        "bottom": Function(enable_command_mode) + Key("L"),
         #NERDTree
+        "open [file]": Key("o"),
         "tree": Function(enable_command_mode) + Text(":NERDTreeToggle") + Key("enter"),
-        "bookmark config": Function(enable_command_mode) + Text(":OpenBookmark vimrc") + Key("enter"),
-        "bookmark macros": Function(enable_command_mode) + Text(":OpenBookmark voiceToCode") + Key("enter"),
+        "bookmark config": Function(enable_command_mode) + Text(":OpenBookmark config") + Key("enter"),
+        "bookmark macros": Function(enable_command_mode) + Text(":OpenBookmark macros") + Key("enter"),
+        "bookmark journal": Function(enable_command_mode) + Text(":OpenBookmark journal") + Key("enter"),
         #windowing
-        "window next": Key("c-w, w"),
+        "[next] window": Function(enable_command_mode) + Key("c-w, w"),
+        "[switch] window": Function(enable_command_mode) + Key("c-w, w"),
         #buffers
-        "buffer list": Function(enable_command_mode) + Text(":ls") + Key("enter")
-    },
+        "buffer list": Function(enable_command_mode) + Text(":ls") + Key("enter"),
+        #tabs
+        "next tab": Function(enable_command_mode) + Key("g, t"),
+        "previous tab": Function(enable_command_mode) + Key("g, T"),
+        "close tab": Function(enable_command_mode) + Text(":tabclose") + Key("enter")
+        },
     extras=[
         IntegerRef("n", 1, 100),
         Dictation("text"),
@@ -71,7 +84,7 @@ grammarCommand.load()
 
 insertMode = MappingRule(
     mapping={
-        "(command mode|press escape)": Function(enable_command_mode),
+        "(command [mode]|press escape)": Function(enable_command_mode),
     },
     extras=[
         IntegerRef("n", 1, 100),
