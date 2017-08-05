e5fa9c96ab33638c0190e750672bb0a9335cf7b3
Isaac Stennett <izikandrw@gmail.com>
2017-08-04 23:40:02 -1000

    home directory modification

--- _miscellaneous.py
+++ _miscellaneous.py
@@ -8,11 +8,13 @@ rules = MappingRule(
       "personal email": Text("izikandrw@gmail.com"),
       "business email": Text("isaac.stennett@goftwaresolutions.com"),
       "daily script": Text("todoist.com"),
+      # filesystem bookmarks
       "desktop": Text("cd /mnt/c/Users/isaac/Desktop/") + Key("enter"),
       "journal": Text("cd /mnt/c/Users/isaac/Google\ Drive/Information/") + Key("enter"),
       "today": Text("/mnt/c/Users/isaac/Google\ Drive/shellScripts/makeNewJournalEntry.sh") + Key("enter"),
       "source code": Text("cd /mnt/c/Users/isaac/Documents/SourceCode") + Key("enter"),
       "macros": Text("cd /mnt/c/NatLink/NatLink/MacroSystem") + Key("enter"),
+      "vim home": Text("cd /home/isaac/.vim") + Key("enter")
     },
     extras = [
         Dictation("text"),
