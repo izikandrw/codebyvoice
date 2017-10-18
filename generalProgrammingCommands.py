f959830193a979dae6e4a4b5c85118d0317a10d4
Isaac <isaac.stennett@gmail.com>
2017-10-18 09:37:41 -1000

    angles command

--- generalProgrammingCommands.py
+++ generalProgrammingCommands.py
@@ -1,5 +1,7 @@
 from dragonfly import *
 
 map = {
-    "brax":                      Text("{}") + Key("left")
+    "brax":                     Text("{}") + Key("left"),
+    "parens":                   Text("()") + Key("left"),
+    "angles":                   Text("<>") + Key("left")
 }
